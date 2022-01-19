9551edbf4fc797a2dcad46ea99da67c0ba673e5b
Piyush Jain <piyushjz0402@gmail.com>
2022-01-20 04:08:18 +0530

    Added stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
-/// I need to import a helper library
-/// from flutter to get content on the screen
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
-/// Define 'main function to run when our app starts
 void main() {
-  // Create a new text widget to show some text
-  // on the screen
-  var app = const MaterialApp(
-    home: Text("Hello there!"),
-  );
-
-  // Take that widget and show it on the screen
-  runApp(app);
+  runApp(App());
 }
